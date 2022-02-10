06856b41bfac0498130c62de56eb2b6275555361
LoveLonelyTime <1390751361@qq.com>
2022-02-10 16:06:46 +0800

    update comments

--- mymath.h
+++ mymath.h
@@ -1,6 +1,13 @@
 #ifndef __H_MYMATH__
 #define __H_MYMATH__
 
+/*
+ * This is my math lib.
+ * Contains functions for addition, subtraction, multiplication and division.
+ *
+ * Author: LoveLonelyTime
+ */
+
 int add(int a, int b);
 int sub(int a, int b);
 int mul(int a, int b);
