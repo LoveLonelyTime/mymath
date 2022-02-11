b0e0c504a7f716bb532c87203d52cc8403c21dba
LoveLonelyTime <1390751361@qq.com>
2022-02-11 20:14:02 +0800

    B updates test.c

--- test.c
+++ test.c
@@ -7,6 +7,21 @@ int main()
         printf("Unexpected results in 'add'");
         return 0;
     }
+    if (sub(3, 4) != -1)
+    {
+        printf("Error in 'sub'");
+        return 0;
+    }
+    if (mul(3, 4) != 12)
+    {
+        printf("Error in 'mul'");
+        return 0;
+    }
+    if (div(3, 4) != 0)
+    {
+        printf("Error in 'div'");
+        return 0;
+    }
     printf("Everything is OK!");
     return 0;
 }
