6c616c10292f8e2cb3eddf0cc763528fd77ec26e
LoveLonelyTime <1390751361@qq.com>
2022-02-11 20:15:51 +0800

    A updates test.c

--- test.c
+++ test.c
@@ -7,6 +7,21 @@ int main()
         printf("Unexpected results in 'add'");
         return 0;
     }
+    if (sub(3, 4) != -1)
+    {
+        printf("Unexpected results in 'sub'");
+        return 0;
+    }
+    if (mul(3, 4) != 12)
+    {
+        printf("Unexpected results in 'mul'");
+        return 0;
+    }
+    if (div(3, 4) != 0)
+    {
+        printf("Unexpected results in 'div'");
+        return 0;
+    }
     printf("Everything is OK!");
     return 0;
 }
